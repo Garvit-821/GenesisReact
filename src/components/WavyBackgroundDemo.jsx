5d63b8602377272b8bbb05f1e126af8e052b0cac
Sushmit Arora <sushmitarora@gmail.com>
2025-08-04 20:41:35 +0530

    Add files via upload

---
+++ src/components/WavyBackgroundDemo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { WavyBackground } from "./ui/wavy-background";
+
+export function WavyBackgroundDemo({ children }) {
+  return (
+    <WavyBackground className="max-w-4xl mx-auto pb-40">
+      {children}
+    </WavyBackground>
+  );
+}
